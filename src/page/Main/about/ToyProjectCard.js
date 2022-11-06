92be900ddd559a4369e512286691ef03e647782b
miogy <jjangrl87@gmail.com>
2022-11-07 02:22:15 +0900

    Modify:ToyProject반응형

--- src/page/Main/about/ToyProjectCard.js
+++ src/page/Main/about/ToyProjectCard.js
@@ -90,115 +90,350 @@ function ToyProjectCard({ projectData }) {
 export default ToyProjectCard;
 
 const StyledToyCard = styled.div`
-  width: 90%;
-  padding-top: 60px;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  overflow: scroll;
-  cursor: pointer;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-  .ToyPrjContainer {
-    position: relative;
-    width: 280px;
-    margin-right: 30px;
-    /* border: 1px solid lightgray; */
-    .cardContainer {
-      padding: 10px;
-      margin: 0 auto;
-      .toyPrjImg {
-        img {
-          width: 100%;
-        }
-      }
-      .toyCardContents {
-        width: 100%;
+  @media screen and (min-width: 980px) {
+    width: 90%;
+    margin: 0 auto;
+    padding-top: 60px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    overflow: scroll;
+    cursor: pointer;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    .ToyPrjContainer {
+      position: relative;
+      width: 280px;
+      margin-right: 30px;
+      /* border: 1px solid lightgray; */
+      .cardContainer {
+        padding: 10px;
         margin: 0 auto;
-
-        .toyPrjTop {
-          display: flex;
-          justify-content: space-around;
-          align-items: center;
-          padding-bottom: 10px;
-          border-bottom: 1px solid lightgray;
-          .title {
-            font-size: 18px;
-            font-weight: 600;
-            color: #333;
-          }
-          button {
-            border: 0;
-            background-color: rgba(0, 0, 0, 0);
+        .toyPrjImg {
+          img {
+            width: 100%;
           }
         }
-        .prjAbout {
-          width: 80%;
-          height: 110px;
+        .toyCardContents {
+          width: 100%;
+          margin: 0 auto;
+
+          .toyPrjTop {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            padding-bottom: 10px;
+            border-bottom: 1px solid lightgray;
+            .title {
+              font-size: 18px;
+              font-weight: 600;
+              color: #333;
+            }
+            button {
+              border: 0;
+              background-color: rgba(0, 0, 0, 0);
+            }
+          }
+          .prjAbout {
+            width: 80%;
+            height: 110px;
+            margin: 0 auto;
+            font-size: 14px;
+            color: #333;
+            overflow: scroll;
+            ::-webkit-scrollbar {
+              display: none;
+            }
+            strong {
+              width: 100%;
+              display: block;
+              margin-bottom: 6px;
+            }
+            .introduce {
+              line-height: 22px;
+            }
+            button {
+              font-size: 12px;
+              margin-left: 6px;
+              border: 1px solid lightgray;
+              background-color: #fff;
+              &:hover {
+                background-color: lightgray;
+                color: #fff;
+              }
+            }
+          }
+          .toyPrjContents {
+            width: 100%;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            .toyPrjBtn {
+              width: 60px;
+              padding: 5px;
+              font-size: 12px;
+              border: 1px solid lightgray;
+              border-radius: 5px;
+              &:hover {
+                background-color: #000;
+                border-color: #000;
+                color: #fff;
+              }
+            }
+          }
+        }
+        .toyPrjLink {
+          width: 100%;
           margin: 0 auto;
           font-size: 14px;
-          color: #333;
-          overflow: scroll;
-          ::-webkit-scrollbar {
-            display: none;
+          margin-bottom: 10px;
+          .icon {
+            margin-right: 6px;
           }
-          strong {
-            width: 100%;
-            display: block;
-            margin-bottom: 6px;
-          }
-          .introduce {
-            line-height: 22px;
-          }
-          button {
-            font-size: 12px;
-            margin-left: 6px;
-            border: 1px solid lightgray;
-            background-color: #fff;
-            &:hover {
-              background-color: lightgray;
-              color: #fff;
-            }
-          }
-        }
-        .toyPrjContents {
-          width: 100%;
-          display: flex;
-          justify-content: space-around;
-          align-items: center;
-          .toyPrjBtn {
-            width: 60px;
-            padding: 5px;
-            font-size: 12px;
-            border: 1px solid lightgray;
-            border-radius: 5px;
-            &:hover {
-              background-color: #000;
-              border-color: #000;
-              color: #fff;
-            }
-          }
-        }
-      }
-      .toyPrjLink {
-        width: 100%;
-        margin: 0 auto;
-        font-size: 14px;
-        margin-bottom: 10px;
-        .icon {
-          margin-right: 6px;
         }
       }
     }
+    .projectIntroduce {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+      z-index: 100;
+    }
   }
-  .projectIntroduce {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    z-index: 100;
+  @media screen and (max-width: 979px) and (min-width: 580px) {
+    width: 90%;
+    margin: 0 auto;
+    padding-top: 60px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    overflow: scroll;
+    cursor: pointer;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    .ToyPrjContainer {
+      position: relative;
+      width: 280px;
+      margin-right: 30px;
+      /* border: 1px solid lightgray; */
+      .cardContainer {
+        padding: 10px;
+        margin: 0 auto;
+        .toyPrjImg {
+          img {
+            width: 100%;
+          }
+        }
+        .toyCardContents {
+          width: 100%;
+          margin: 0 auto;
+
+          .toyPrjTop {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            padding-bottom: 10px;
+            border-bottom: 1px solid lightgray;
+            .title {
+              font-size: 18px;
+              font-weight: 600;
+              color: #333;
+            }
+            button {
+              border: 0;
+              background-color: rgba(0, 0, 0, 0);
+            }
+          }
+          .prjAbout {
+            width: 80%;
+            height: 0px;
+            margin: 0 auto;
+            font-size: 14px;
+            line-height: 16px;
+            color: #333;
+            overflow: scroll;
+            ::-webkit-scrollbar {
+              display: none;
+            }
+            strong {
+              width: 100%;
+              display: block;
+              margin-bottom: 6px;
+            }
+            .introduce {
+              line-height: 22px;
+            }
+            button {
+              font-size: 12px;
+              margin-left: 6px;
+              border: 1px solid lightgray;
+              background-color: #fff;
+              &:hover {
+                background-color: lightgray;
+                color: #fff;
+              }
+            }
+          }
+          .toyPrjContents {
+            width: 100%;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            .toyPrjBtn {
+              width: 50px;
+              margin: 0 5px 0 5px;
+              padding: 5px;
+              font-size: 12px;
+              border: 1px solid lightgray;
+              border-radius: 5px;
+              &:hover {
+                background-color: #000;
+                border-color: #000;
+                color: #fff;
+              }
+            }
+          }
+        }
+        .toyPrjLink {
+          width: 100%;
+          margin: 0 auto;
+          font-size: 14px;
+          margin-bottom: 10px;
+          .icon {
+            margin-right: 6px;
+          }
+        }
+      }
+    }
+    .projectIntroduce {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+      z-index: 100;
+    }
+  }
+  @media screen and (max-width: 579px) and (min-width: 230px) {
+    width: 90%;
+    margin: 0 auto;
+    padding-top: 60px;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    overflow: scroll;
+    cursor: pointer;
+    ::-webkit-scrollbar {
+      display: none;
+    }
+    .ToyPrjContainer {
+      position: relative;
+      width: 280px;
+      margin-right: 30px;
+      /* border: 1px solid lightgray; */
+      .cardContainer {
+        padding: 10px;
+        margin: 0 auto;
+        .toyPrjImg {
+          img {
+            width: 100%;
+          }
+        }
+        .toyCardContents {
+          width: 100%;
+          margin: 0 auto;
+
+          .toyPrjTop {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            padding-bottom: 10px;
+            border-bottom: 1px solid lightgray;
+            .title {
+              font-size: 18px;
+              font-weight: 600;
+              color: #333;
+            }
+            button {
+              border: 0;
+              background-color: rgba(0, 0, 0, 0);
+            }
+          }
+          .prjAbout {
+            width: 80%;
+            height: 0px;
+            margin: 0 auto;
+            font-size: 14px;
+            line-height: 16px;
+            color: #333;
+            overflow: scroll;
+            ::-webkit-scrollbar {
+              display: none;
+            }
+            strong {
+              width: 100%;
+              display: block;
+              margin-bottom: 6px;
+            }
+            .introduce {
+              line-height: 22px;
+            }
+            button {
+              font-size: 12px;
+              margin-left: 6px;
+              border: 1px solid lightgray;
+              background-color: #fff;
+              &:hover {
+                background-color: lightgray;
+                color: #fff;
+              }
+            }
+          }
+          .toyPrjContents {
+            width: 100%;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            .toyPrjBtn {
+              width: 50px;
+              margin: 0 5px 0 5px;
+              padding: 5px;
+              font-size: 12px;
+              border: 1px solid lightgray;
+              border-radius: 5px;
+              &:hover {
+                background-color: #000;
+                border-color: #000;
+                color: #fff;
+              }
+            }
+          }
+        }
+        .toyPrjLink {
+          width: 100%;
+          margin: 0 auto;
+          font-size: 14px;
+          margin-bottom: 10px;
+          .icon {
+            margin-right: 6px;
+          }
+        }
+      }
+    }
+    .projectIntroduce {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+      z-index: 100;
+    }
   }
 `;
--- src/page/Main/about/ToyProjectDetail.js
+++ src/page/Main/about/ToyProjectDetail.js
@@ -87,110 +87,469 @@ function ToyProjectDetail({ project, setToyPrjModal, introduce }) {
 export default ToyProjectDetail;
 
 const StyledToyPrj = styled.div`
-  width: 100%;
-  .toyPrjWrap {
-    position: relative;
-    width: 1100px;
-    height: 100vh;
-    margin: 0 auto;
-    overflow: scroll;
-    z-index: 9900;
-    ::-webkit-scrollbar {
-      display: none;
-    }
-    button {
-      position: fixed;
-      top: 70px;
-      right: 10%;
-      width: 80px;
-      height: 36px;
-      margin-right: 50px;
-      margin-bottom: 80px;
-      border: 1px solid lightgray;
-      border-radius: 5px;
-      background-color: rgba(255, 255, 255, 0.6);
-      color: #333;
-      z-index: 9999;
-      &:hover {
-        color: #be0a26;
-        cursor: pointer;
-        font-weight: 600;
+  @media screen and (min-width: 1100px) {
+    width: 100%;
+    .toyPrjWrap {
+      position: relative;
+      width: 980px;
+      height: 100vh;
+      margin: 0 auto;
+      overflow: scroll;
+      z-index: 9900;
+      ::-webkit-scrollbar {
+        display: none;
       }
-    }
-    .listWrap {
-      /* width: 980px; */
-      padding-top: 70px;
-      h4 {
-        font-size: 32px;
+      button {
+        position: fixed;
+        top: 70px;
+        right: 10%;
+        width: 80px;
+        height: 36px;
+        margin-right: 50px;
+        margin-bottom: 80px;
+        border: 1px solid lightgray;
+        border-radius: 5px;
+        background-color: rgba(255, 255, 255, 0.6);
         color: #333;
-        display: flex;
-        align-items: center;
-        .projectBtn {
+        z-index: 9999;
+        &:hover {
+          color: #be0a26;
+          cursor: pointer;
+          font-weight: 600;
+        }
+      }
+      .listWrap {
+        /* width: 980px; */
+        padding-top: 70px;
+        h4 {
+          font-size: 32px;
+          color: #333;
           display: flex;
           align-items: center;
-          margin-left: 10px;
-          span {
-            font-size: 12px;
-            margin-left: 20px;
-            padding: 5px;
-            border: 1px solid lightgray;
-            border-radius: 5px;
-            color: lightgray;
-            cursor: pointer;
-            &:hover {
-              background-color: #000;
-              color: #fff;
-              border-color: #000;
+          .projectBtn {
+            display: flex;
+            align-items: center;
+            margin-left: 10px;
+            span {
+              font-size: 12px;
+              margin-left: 20px;
+              padding: 5px;
+              border: 1px solid lightgray;
+              border-radius: 5px;
+              color: lightgray;
+              cursor: pointer;
+              &:hover {
+                background-color: #000;
+                color: #fff;
+                border-color: #000;
+              }
+            }
+          }
+        }
+        .introduceWrap {
+          width: 90%;
+          margin: 0 auto;
+          line-height: 26px;
+          margin-bottom: 10px;
+          text-align: start;
+          border-bottom: 1px solid lightgray;
+          h5 {
+            font-size: 18px;
+          }
+        }
+        .realizeContents {
+          margin: 0 auto;
+          text-align: start;
+          border-bottom: 1px solid lightgray;
+          .contentsWrap {
+            width: 90%;
+            margin: 0 auto;
+            padding-top: 30px;
+            ul {
+              width: 860px;
+              li {
+                font-size: 15px;
+                list-style: none;
+                text-decoration: none;
+                line-height: 26px;
+                color: #666;
+                img {
+                  width: 100%;
+                }
+              }
+              .contentsTitle {
+                font-size: 18px;
+                color: #000;
+                font-weight: 600;
+                line-height: 60px;
+              }
+              .gifTitle {
+                padding: 30px;
+                font-size: 16px;
+                color: #000;
+                font-weight: bold;
+              }
+              .final {
+                width: 600px;
+                margin: 0 auto;
+              }
             }
           }
         }
       }
-      .introduceWrap {
-        width: 90%;
-        margin: 0 auto;
-        line-height: 26px;
-        margin-bottom: 10px;
-        text-align: start;
-        border-bottom: 1px solid lightgray;
-        h5 {
-          font-size: 18px;
+    }
+  }
+  @media screen and (max-width: 1099px) and (min-width: 980px) {
+    width: 100%;
+    .toyPrjWrap {
+      position: relative;
+      width: 800px;
+      height: 100vh;
+      margin: 0 auto;
+      overflow: scroll;
+      z-index: 9900;
+      ::-webkit-scrollbar {
+        display: none;
+      }
+      button {
+        position: fixed;
+        top: 70px;
+        right: 10%;
+        width: 80px;
+        height: 36px;
+        margin-right: 50px;
+        margin-bottom: 80px;
+        border: 1px solid lightgray;
+        border-radius: 5px;
+        background-color: rgba(255, 255, 255, 0.6);
+        color: #333;
+        z-index: 9999;
+        &:hover {
+          color: #be0a26;
+          cursor: pointer;
+          font-weight: 600;
         }
       }
-      .realizeContents {
-        margin: 0 auto;
-        text-align: start;
-        border-bottom: 1px solid lightgray;
-        .contentsWrap {
-          width: 90%;
-          margin: 0 auto;
-          padding-top: 30px;
-          ul {
-            width: 860px;
-            li {
-              font-size: 15px;
-              list-style: none;
-              text-decoration: none;
-              line-height: 26px;
-              color: #666;
-              img {
-                width: 100%;
+      .listWrap {
+        /* width: 980px; */
+        padding-top: 70px;
+        h4 {
+          font-size: 32px;
+          color: #333;
+          display: flex;
+          align-items: center;
+          .projectBtn {
+            display: flex;
+            align-items: center;
+            margin-left: 10px;
+            span {
+              font-size: 12px;
+              margin-left: 20px;
+              padding: 5px;
+              border: 1px solid lightgray;
+              border-radius: 5px;
+              color: lightgray;
+              cursor: pointer;
+              &:hover {
+                background-color: #000;
+                color: #fff;
+                border-color: #000;
               }
             }
-            .contentsTitle {
-              font-size: 18px;
-              color: #000;
-              font-weight: 600;
-              line-height: 60px;
-            }
-            .gifTitle {
-              padding: 30px;
-              font-size: 16px;
-              color: #000;
-              font-weight: bold;
-            }
-            .final {
-              width: 600px;
+          }
+        }
+        .introduceWrap {
+          width: 90%;
+          margin: 0 auto;
+          line-height: 26px;
+          margin-bottom: 10px;
+          text-align: start;
+          border-bottom: 1px solid lightgray;
+          h5 {
+            font-size: 18px;
+          }
+        }
+        .realizeContents {
+          margin: 0 auto;
+          text-align: start;
+          border-bottom: 1px solid lightgray;
+          .contentsWrap {
+            width: 90%;
+            margin: 0 auto;
+            padding-top: 20px;
+            ul {
+              width: 700px;
               margin: 0 auto;
+              li {
+                width: 90%;
+                font-size: 15px;
+                list-style: none;
+                text-decoration: none;
+                line-height: 22px;
+                color: #666;
+                img {
+                  width: 100%;
+                }
+              }
+              .contentsTitle {
+                font-size: 18px;
+                color: #000;
+                font-weight: 600;
+                line-height: 60px;
+              }
+              .gifTitle {
+                padding: 30px;
+                font-size: 16px;
+                color: #000;
+                font-weight: bold;
+              }
+              .final {
+                width: 600px;
+                margin: 0 auto;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 979px) and (min-width: 580px) {
+    width: 100%;
+    .toyPrjWrap {
+      position: relative;
+      width: 570px;
+      height: 100vh;
+      margin: 0 auto;
+      overflow: scroll;
+      z-index: 9900;
+      ::-webkit-scrollbar {
+        display: none;
+      }
+      button {
+        position: fixed;
+        top: 70px;
+        right: 0;
+        width: 70px;
+        height: 32px;
+        margin-right: 30px;
+        margin-bottom: 80px;
+        border: 1px solid lightgray;
+        border-radius: 5px;
+        background-color: rgba(255, 255, 255, 0.6);
+        color: #333;
+        z-index: 9999;
+        &:hover {
+          color: #be0a26;
+          cursor: pointer;
+          font-weight: 600;
+        }
+      }
+      .listWrap {
+        /* width: 980px; */
+        padding-top: 60px;
+        h4 {
+          font-size: 32px;
+          color: #333;
+          display: flex;
+          align-items: center;
+          .projectBtn {
+            display: flex;
+            align-items: center;
+            margin-left: 10px;
+            span {
+              font-size: 12px;
+              margin-left: 20px;
+              padding: 5px;
+              border: 1px solid lightgray;
+              border-radius: 5px;
+              color: lightgray;
+              cursor: pointer;
+              &:hover {
+                background-color: #000;
+                color: #fff;
+                border-color: #000;
+              }
+            }
+          }
+        }
+        .introduceWrap {
+          width: 90%;
+          margin: 0 auto;
+          line-height: 22px;
+          text-align: start;
+          padding-bottom: 10px;
+          h5 {
+            font-size: 18px;
+          }
+          p {
+            margin: 3px 0 3px 0;
+          }
+        }
+        .realizeContents {
+          margin: 0 auto;
+          text-align: start;
+          border-top: 1px solid lightgray;
+          border-bottom: 1px solid lightgray;
+          .contentsWrap {
+            width: 100%;
+            margin: 0 auto;
+            padding-top: 20px;
+            ul {
+              width: 560px;
+              margin: 0 auto;
+              li {
+                width: 90%;
+                margin: 0;
+                font-size: 15px;
+                list-style: none;
+                text-decoration: none;
+                line-height: 22px;
+                color: #666;
+                img {
+                  width: 480px;
+                }
+              }
+              .contentsTitle {
+                font-size: 18px;
+                color: #000;
+                font-weight: 600;
+                line-height: 60px;
+              }
+              .gifTitle {
+                padding: 30px;
+                font-size: 16px;
+                color: #000;
+                font-weight: bold;
+              }
+              .final {
+                width: 600px;
+                margin: 0 auto;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  @media screen and (max-width: 579px) and (min-width: 230px) {
+    width: 100%;
+    .toyPrjWrap {
+      position: relative;
+      width: 90%;
+      height: 100vh;
+      margin: 0 auto;
+      overflow: scroll;
+      z-index: 9900;
+      ::-webkit-scrollbar {
+        display: none;
+      }
+      .projectBtn {
+        width: 100%;
+        display: block;
+      }
+      button {
+        position: fixed;
+        top: 70px;
+        right: 0;
+        width: 70px;
+        height: 32px;
+        margin-right: 30px;
+        margin-bottom: 80px;
+        border: 1px solid lightgray;
+        border-radius: 5px;
+        background-color: rgba(255, 255, 255, 0.6);
+        color: #333;
+        z-index: 9999;
+        &:hover {
+          color: #be0a26;
+          cursor: pointer;
+          font-weight: 600;
+        }
+      }
+      .listWrap {
+        /* width: 980px; */
+        padding-top: 60px;
+        h4 {
+          margin: 0;
+          font-size: 28px;
+          color: #333;
+          display: flex;
+          align-items: center;
+          flex-wrap: wrap;
+          .projectBtn {
+            display: flex;
+            align-items: center;
+            margin: 10px;
+            span {
+              font-size: 12px;
+              margin-left: 20px;
+              padding: 5px;
+              border: 1px solid lightgray;
+              border-radius: 5px;
+              color: lightgray;
+              cursor: pointer;
+              &:hover {
+                background-color: #000;
+                color: #fff;
+                border-color: #000;
+              }
+            }
+          }
+        }
+        .introduceWrap {
+          width: 90%;
+          margin: 0 auto;
+          line-height: 22px;
+          text-align: start;
+          padding-bottom: 10px;
+          h5 {
+            font-size: 16px;
+          }
+          p {
+            font-size: 14px;
+            margin: 3px 0 3px 0;
+          }
+        }
+        .realizeContents {
+          margin: 0 auto;
+          text-align: start;
+          border-top: 1px solid lightgray;
+          border-bottom: 1px solid lightgray;
+          .contentsWrap {
+            width: 100%;
+            margin: 0 auto;
+            padding-top: 20px;
+            ul {
+              padding: 0;
+              li {
+                width: 90%;
+                margin: 0;
+                padding: 0 10px 0 10px;
+                font-size: 14px;
+                list-style: none;
+                text-decoration: none;
+                line-height: 20px;
+                color: #666;
+                img {
+                  width: 100%;
+                }
+              }
+              .contentsTitle {
+                font-size: 16px;
+                color: #000;
+                font-weight: 600;
+                line-height: 30px;
+              }
+              .gifTitle {
+                padding: 30px;
+                font-size: 16px;
+                color: #000;
+                font-weight: bold;
+              }
+              .final {
+                width: 100%;
+                margin: 0 auto;
+              }
             }
           }
         }
